7b8fe51747bca0ac29439a438e296c3cfd82c46b
league <league@imac-14.attlocal.net>
2022-11-26 12:58:35 -0800

    On Frogger step 12

--- src/_06_frogger/Car.java
+++ src/_06_frogger/Car.java
@@ -1,11 +1,24 @@
 package _06_frogger;
 
-public class Car {
+
+
+public class Car{
 	private int xPos;
 	private int yPos;
 	private int spd;
 	private int size;
-	public Car() {
+	
+	public Car(int x,int y,int s,int si) {
+		xPos =x;
+		yPos = y;
+		spd =s;
+		size =si;
 		
 	}
+	void display(){
+	    Frogger.window.fill(0,255,0);
+	    Frogger.window.rect(xPos , yPos,  size, 50);
+	}
+	
 }
+
--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -3,11 +3,17 @@ package _06_frogger;
 import processing.core.PApplet;
 
 public class Frogger extends PApplet {
-    static final int WIDTH = 600;
-    static final int HEIGHT = 400;
+    static final int WIDTH = 800;
+    static final int HEIGHT = 600;
+    static Frogger window;
     int hop;
     int x =300;
     int y= 390;
+    Car c1;
+    Car c2;
+    Car c3;
+    Car c4;
+    Car c5;
     
     @Override
     public void settings() {
@@ -16,12 +22,22 @@ public class Frogger extends PApplet {
 
     @Override
     public void setup() {
-    	
+    	c1 = new Car(0,0,100,100);
+    	c2 = new Car(0,120,10,100);
+    	c3 = new Car(0,240,10,100);
+    	c4 = new Car(0,360,10,100);
+    	c5 = new Car(0,480,10,100);
+    	window=this;
     }
 
     @Override
     public void draw() {
     	background(0,0,255);
+    	c1.display();
+    	c2.display();
+    	c3.display();
+    	c4.display();
+    	c5.display();
     	fill(0, 255, 0);
     	ellipse(x,y, 25, 25);
     	keyPressed();
@@ -29,28 +45,29 @@ public class Frogger extends PApplet {
     }
     public void keyPressed() {
     	if(key == CODED){
-    	        if(keyCode == UP)
+    	        if(keyCode == UP&&y>=0)
     	        {
     	            //Frog Y position goes up
     	        	y--;
     	        }
-    	        else if(keyCode == DOWN)
+    	        else if(keyCode == DOWN&&y<=HEIGHT)
     	        {
     	            //Frog Y position goes down 
     	        	y++;
     	        }
-    	        else if(keyCode == RIGHT)
+    	        else if(keyCode == RIGHT&&x<=WIDTH)
     	        {
     	            //Frog X position goes right
     	        	x++;
     	        }
-    	        else if(keyCode == LEFT)
+    	        else if(keyCode == LEFT&&x>=0)
     	        {
     	            //Frog X position goes left
     	        	x--;
     	        }
     	    }    
     }
+  
 
     static public void main(String[] args) {
         PApplet.main(Frogger.class.getName());
